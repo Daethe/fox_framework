bbde53be17ea9158aa90c487301c5edb3bb66b4f
Marc Proux <daethe.pro@gmail.com>
2017-02-19 02:35:25 +0100

    Some exception added

---
+++ src/Core/Exception/InvalidDatabaseTypeException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marcp
+ * Date: 19/02/2017
+ * Time: 01:12
+ */
+
+namespace Core\Exception;
+
+use \Exception;
+
+class InvalidDatabaseTypeException extends Exception {}
