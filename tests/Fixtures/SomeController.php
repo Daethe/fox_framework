f9c7bd44e77d6b9a969f8f85cefdde9f0c7ae31a
Marc Proux <daethe.pro@gmail.com>
2017-02-19 22:31:20 +0100

    Added some fixtures

--- tests/Fixtures/SomeController.php
+++ tests/Fixtures/SomeController.php
@@ -2,6 +2,26 @@
 
 namespace CoreTests\Fixtures;
 
-class SomeController {
+final class SomeController {
+
+	public function usersCreate() {}
+
+	public function indexAction() {}
+
+	public function user() {}
+
+	/**
+	 * @return mixed[]
+	 */
+	public function page() {
+		return func_get_args();
+	}
+
+	/**
+	 * @return mixed[]
+	 */
+	public function dynamicFilterUrlMatch() {
+		return func_get_args();
+	}
 
 }
---
+++ tests/Fixtures/TestController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marcp
+ * Date: 19/02/2017
+ * Time: 19:06
+ */
+
+namespace CoreTests\Fixtures;
+
+
+use Core\Controller\Controller;
+
+class TestController extends Controller{
+
+}
