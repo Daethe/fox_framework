345735e1cf17cee808138be0d5383b99cedc37af
Marc Proux <daethe.pro@gmail.com>
2017-03-07 23:26:45 +0100

    Some Exception

---
+++ src/Core/Exception/UnknowTypeException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marcp
+ * Date: 07/03/2017
+ * Time: 22:05
+ */
+
+namespace Core\Exception;
+
+use \Exception;
+
+class UnknownTypeException extends \Exception {}
---
+++ src/Core/Exception/UnknownDirException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marcp
+ * Date: 07/03/2017
+ * Time: 22:13
+ */
+
+namespace Core\Exception;
+
+use \Exception;
+
+class UnknownDirException extends Exception {}
