6a913d37aeb88ae5c7caf9625f7b921709d2cb61
Marc Proux <daethe.pro@gmail.com>
2017-03-07 23:27:02 +0100

    Modification for Assets Sample

--- src/App/Views/layouts/default.php
+++ src/App/Views/layouts/default.php
@@ -1,10 +1,16 @@
+<?php
+use \App\App;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <title>Light framework</title>
+		<?= App::getAssets()->CSS()->dump() ?>
     </head>
     <body>
         <?= $content ?>
+		<?= App::getAssets()->JS()->dump() ?>
     </body>
 </html>
