3741ee4a44d439442e5ccabbfa1cfbc2a5ffe678
Marc Proux <daethe.pro@gmail.com>
2017-03-09 22:44:57 +0100

    Commented Server class

--- src/Core/Web/Server/Server.php
+++ src/Core/Web/Server/Server.php
@@ -1,30 +1,48 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: marcp
- * Date: 19/02/2017
- * Time: 16:50
- */
 
 namespace Core\Web\Server;
 
 
 class Server {
 
+	/**
+	 * @type array Contain SUPERGLOBAL $_SERVER variable
+	 */
 	private $serverGlobal;
 
+	/**
+	 * Server constructor.
+	 */
 	public function __construct() {
 		$this->serverGlobal = $_SERVER;
 	}
 
+	/**
+	 * Get an interface variable
+	 *
+	 * @param $interface
+	 *
+	 * @return mixed
+	 */
 	public function get($interface) {
 		return $this->serverGlobal[$interface];
 	}
 
+	/**
+	 * Edit an interface variable
+	 *
+	 * @param $interface
+	 * @param $value
+	 */
 	public function set($interface, $value) {
 		$this->serverGlobal[$interface] = $value;
 	}
 
+	/**
+	 * Get the SUPERGLOBAL variable
+	 *
+	 * @return array
+	 */
 	public function getServer() {
 		return $this->serverGlobal;
 	}
