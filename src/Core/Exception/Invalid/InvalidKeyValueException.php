10eb35135e76d11c1c7dd9b4b7b7fa8f163ecdb6
Marc Proux <daethe.pro@gmail.com>
2017-03-10 01:01:02 +0100

    Some Exception

---
+++ src/Core/Exception/Invalid/InvalidKeyValueException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marcp
+ * Date: 09/03/2017
+ * Time: 22:29
+ */
+
+namespace Core\Exception;
+
+
+class InvalidKeyValueException extends \Exception {}
---
+++ src/Core/Exception/Not/NotAnArrayException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marcp
+ * Date: 09/03/2017
+ * Time: 22:30
+ */
+
+namespace Core\Exception;
+
+
+class NotAnArrayException extends \Exception {}
